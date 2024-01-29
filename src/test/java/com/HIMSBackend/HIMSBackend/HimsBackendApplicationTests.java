4962843432986fc1282795120d5c33d7abbe339e
Vinay-Vastrakar <vinay.vastrakar@gloitel.in>
2024-01-29 12:59:32 +0530

    first intial setup

---
+++ src/test/java/com/HIMSBackend/HIMSBackend/HimsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HIMSBackend.HIMSBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
