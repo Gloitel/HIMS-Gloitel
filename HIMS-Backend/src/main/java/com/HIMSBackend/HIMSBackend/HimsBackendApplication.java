65c5381797d4bfcb7545697f1b8b5b0ebcf300de
Vinay-Vastrakar <vinay.vastrakar@gloitel.in>
2024-01-29 12:35:58 +0530

    Initial setup 2

---
+++ HIMS-Backend/src/main/java/com/HIMSBackend/HIMSBackend/HimsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.HIMSBackend.HIMSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimsBackendApplication.class, args);
+		System.out.println("Vinay");
+	}
+
+}
